ce208c35046fbd1108c76630fa843d3d7dd3bfe3
CalebRose <ctrose17@gmail.com>
2021-04-30 17:04:01 -0700

    Time stamp functions for later controls

--- structs/timestamp.go
+++ structs/timestamp.go
@@ -12,7 +12,47 @@ type Timestamp struct {
 	NBAWeek           int
 	GamesARan         bool
 	GamesBRan         bool
+	GamesCRan         bool
 	RecruitingSynced  bool
 	GMActionsComplete bool
 	IsOffSeason       bool
+}
+
+func (t *Timestamp) MoveUpWeekCollege() {
+	t.CollegeWeekID++
+}
+
+func (t *Timestamp) MoveUpWeekNBA() {
+	t.NBAWeekID++
+}
+
+func (t *Timestamp) ToggleGamesARan() {
+	t.GamesARan = !t.GamesARan
+}
+
+func (t *Timestamp) ToggleGamesBRan() {
+	t.GamesBRan = !t.GamesBRan
+}
+
+func (t *Timestamp) ToggleGamesCRan() {
+	t.GamesCRan = !t.GamesCRan
+}
+
+func (t *Timestamp) ToggleRecruiting() {
+	t.RecruitingSynced = !t.RecruitingSynced
+}
+
+func (t *Timestamp) ToggleGMActions() {
+	t.GMActionsComplete = !t.GMActionsComplete
+}
+
+func (t *Timestamp) SyncToNextWeek() {
+	t.MoveUpWeekCollege()
+	t.MoveUpWeekNBA()
+	// Reset Toggles
+	t.ToggleGamesARan()
+	t.ToggleGamesBRan()
+	t.ToggleGamesCRan()
+	t.ToggleRecruiting()
+	t.ToggleGMActions()
 }
