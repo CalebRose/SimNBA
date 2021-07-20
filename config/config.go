3830d60d13832ed09a80b4117379d5bc34c94b11
CalebRose <ctrose17@gmail.com>
2021-07-19 21:29:17 -0700

    .gitignore fixed

--- config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-func Config() map[string]string {
-	return map[string]string{
-		"db": "mysql",
-		"cs": "simfbaah_davidross10:bestpunterev3r!@tcp(68.66.216.54)/simfbaah_simnba?parseTime=true",
-	}
-}
