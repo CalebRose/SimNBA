aa9cfa5168555c39f8d140360c73adf16cd00c97
CalebRose <ctrose17@gmail.com>
2023-02-13 07:06:16 -0800

    more headers

--- controller/AdminController.go
+++ controller/AdminController.go
@@ -13,6 +13,8 @@ import (
 
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
 }
 
 func GeneratePlayers(w http.ResponseWriter, r *http.Request) {
