708eec60edd19bba38e48937bb8722069f9aa41c
CalebRose <ctrose17@gmail.com>
2023-07-05 16:16:36 -0700

    part 1 of matches algorithm

--- managers/matchManager.go
+++ managers/matchManager.go
@@ -8,6 +8,101 @@ import (
 	"github.com/CalebRose/SimNBA/structs"
 )
 
+func GetMatchesForTimeslot() []structs.MatchResponse {
+	ts := GetTimestamp()
+	seasonID := strconv.Itoa(int(ts.SeasonID))
+	weekID := strconv.Itoa(int(ts.CollegeWeekID))
+	nbaWeekID := strconv.Itoa(int(ts.NBAWeekID))
+
+	matchType := ""
+
+	if !ts.GamesARan {
+		matchType = "A"
+	} else if !ts.GamesBRan {
+		matchType = "B"
+	} else if !ts.GamesCRan {
+		matchType = "C"
+	} else if !ts.GamesDRan {
+		matchType = "D"
+	} else {
+		panic("STOP")
+	}
+
+	matchesList := []structs.MatchResponse{}
+
+	// Get College Matches
+	collegeMatches := GetMatchesByWeekId(weekID, seasonID, matchType)
+
+	for _, c := range collegeMatches {
+		if c.GameComplete {
+			continue
+		}
+
+		match := structs.MatchResponse{
+			MatchName:              c.MatchName,
+			ID:                     c.ID,
+			WeekID:                 c.WeekID,
+			Week:                   c.Week,
+			SeasonID:               c.SeasonID,
+			HomeTeamID:             c.HomeTeamID,
+			HomeTeam:               c.HomeTeam,
+			AwayTeamID:             c.AwayTeamID,
+			AwayTeam:               c.AwayTeam,
+			MatchOfWeek:            c.MatchOfWeek,
+			Arena:                  c.Arena,
+			City:                   c.City,
+			State:                  c.State,
+			IsNeutralSite:          c.IsNeutralSite,
+			IsNBAMatch:             false,
+			IsConference:           c.IsConference,
+			IsConferenceTournament: c.IsConferenceTournament,
+			IsNILGame:              c.IsNILGame,
+			IsPlayoffGame:          c.IsPlayoffGame,
+			IsNationalChampionship: c.IsNationalChampionship,
+			IsRivalryGame:          c.IsRivalryGame,
+		}
+
+		matchesList = append(matchesList, match)
+	}
+
+	// Get Professional Matches
+	nbaMatches := GetNBATeamMatchesByMatchType(nbaWeekID, seasonID, matchType)
+
+	for _, n := range nbaMatches {
+		if n.GameComplete {
+			continue
+		}
+
+		match := structs.MatchResponse{
+			MatchName:              n.MatchName,
+			ID:                     n.ID,
+			WeekID:                 n.WeekID,
+			Week:                   n.Week,
+			SeasonID:               n.SeasonID,
+			HomeTeamID:             n.HomeTeamID,
+			HomeTeam:               n.HomeTeam,
+			AwayTeamID:             n.AwayTeamID,
+			AwayTeam:               n.AwayTeam,
+			MatchOfWeek:            n.MatchOfWeek,
+			Arena:                  n.Arena,
+			City:                   n.City,
+			State:                  n.State,
+			IsNeutralSite:          n.IsNeutralSite,
+			IsNBAMatch:             true,
+			IsConference:           n.IsConference,
+			IsConferenceTournament: n.IsConferenceTournament,
+			IsNILGame:              n.IsNILGame,
+			IsPlayoffGame:          n.IsPlayoffGame,
+			IsNationalChampionship: n.IsTheFinals,
+			IsRivalryGame:          n.IsRivalryGame,
+		}
+
+		matchesList = append(matchesList, match)
+	}
+
+	return matchesList
+}
+
 func GetMatchesByTeamIdAndSeasonId(teamId string, seasonId string) []structs.Match {
 	db := dbprovider.GetInstance().GetDB()
 
@@ -92,6 +187,16 @@ func GetTeamMatchesByWeekId(weekId, seasonID, matchType, teamID string) []struct
 	var teamMatches []structs.Match
 
 	db.Where("week_id = ? AND season_id = ? AND match_of_week = ? AND (home_team_id = ? OR away_team_id = ?)", weekId, seasonID, matchType, teamID, teamID).Find(&teamMatches)
+
+	return teamMatches
+}
+
+func GetNBATeamMatchesByMatchType(weekId, seasonID, matchType string) []structs.NBAMatch {
+	db := dbprovider.GetInstance().GetDB()
+
+	var teamMatches []structs.NBAMatch
+
+	db.Where("week_id = ? AND season_id = ? AND match_of_week = ?", weekId, seasonID, matchType).Find(&teamMatches)
 
 	return teamMatches
 }
