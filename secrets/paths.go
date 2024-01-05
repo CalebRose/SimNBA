ada376fa8487ae16cea410062ba989dc90e996a6
CalebRose <ctrose17@gmail.com>
2024-01-05 09:48:38 -0800

    Revealing the secrets folder (literally just for getting paths)

---
+++ secrets/paths.go
@@ -0,0 +1,26 @@
+package secrets
+
+func GetPath() map[string]string {
+	TeamsListPath := "./data/teamslist_v5.csv"
+	NBAStandingsPath := "./NBAStandings.csv"
+	NBATeamsPath := "./NBATeams.csv"
+	ArenaPath := "./data/Arenas.csv"
+	DraftLotteryPath := "./data/DraftStandings.csv"
+	aiBehaviorPath := "./data/NewAIBehaviors.csv"
+	extensionsPath := "./data/TempExtensions.csv"
+	cbbMatchPath := "./data/2023_CBB_Season.csv"
+	nbaMatchPath := "./data/2023_SimNBA_Season.csv"
+	draftPickPath := "./data/draft_picks.csv"
+	return map[string]string{
+		"teams":        TeamsListPath,
+		"nbastandings": NBAStandingsPath,
+		"nbateams":     NBATeamsPath,
+		"arenas":       ArenaPath,
+		"draftlottery": DraftLotteryPath,
+		"ai":           aiBehaviorPath,
+		"extensions":   extensionsPath,
+		"cbbmatches":   cbbMatchPath,
+		"nbamatches":   nbaMatchPath,
+		"draftpicks":   draftPickPath,
+	}
+}
