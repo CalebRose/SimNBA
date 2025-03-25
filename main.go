ddc353d161183186c6f45a11dbdb51c64eac053e
CalebRose <ctrose17@gmail.com>
2025-03-25 06:49:08 -0700

    updating cron jobs

--- main.go
+++ main.go
@@ -354,13 +354,17 @@ func loadEnvs() {
 func handleCron() *cron.Cron {
 
 	c := cron.New()
-	c.AddFunc("0 16 * * 3", controller.SyncRecruitingViaCron)
-	c.AddFunc("0 6 * * 4,6", controller.SyncAIBoardsViaCron)
-	c.AddFunc("0 20 * * 1,3,5,6", controller.ShowGamesViaCron)
-	c.AddFunc("0 22 * * 1,3,5,6", controller.RunAIGameplansViaCron)
-	c.AddFunc("0 10 * * 4", controller.FillAIBoardsViaCron)
-	c.AddFunc("0 12 * * 0", controller.SyncToNextWeekViaCron)
-	c.AddFunc("0 16 * * 2", controller.SyncFreeAgencyOffersViaCron)
+	runJobs := os.Getenv("RUN_JOBS")
+	if runJobs != "FALSE" {
+		c.AddFunc("0 16 * * 3", controller.SyncRecruitingViaCron)
+		c.AddFunc("0 6 * * 4,6", controller.SyncAIBoardsViaCron)
+		c.AddFunc("0 20 * * 1,3,5,6", controller.ShowGamesViaCron)
+		c.AddFunc("0 22 * * 1,3,5,6", controller.RunAIGameplansViaCron)
+		c.AddFunc("0 10 * * 4", controller.FillAIBoardsViaCron)
+		c.AddFunc("0 12 * * 0", controller.SyncToNextWeekViaCron)
+		c.AddFunc("0 16 * * 2", controller.SyncFreeAgencyOffersViaCron)
+	}
+
 	c.Start()
 
 	return c
